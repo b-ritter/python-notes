6ea913dc0f011ca6771b9f8b9d63c2c60df3a555
RITTER <br440r@att.com>
2017-05-04 13:27:03 -0500

    Make module to return list of years (as string) from inputs. Include tests

--- years/test_years.py
+++ years/test_years.py
@@ -7,5 +7,9 @@ class TestYears(unittest.TestCase):
     def test_years_returns_correct_values(self):
         self.assertListEqual(years(2000, 2003), ['2000', '2001', '2002', '2003'])
 
+    def test_years_too_far_in_past(self):
+        with years(1900, 2001):
+            self.assertRaises(ValueError)
+
 if __name__ == "__main__":
     unittest.main()
--- years/years_as_string_list.py
+++ years/years_as_string_list.py
@@ -1,8 +1,17 @@
 
 def years(start, end):
-    """ Build a list of years as strings."""
+    """ Build a list of years in the recent past/future as strings."""
     end = end + 1
-    return [str(yr) for yr in range(start, end)]
+    try:
+        too_far_in_past = start >= 2000
+        too_far_in_future = end <= 2020
+        if too_far_in_future or too_far_in_past:
+            raise ValueError
+    except ValueError as e:
+        print(e)
+        raise
+    else:
+        return [str(yr) for yr in range(start, end)]
 
 if __name__ == '__main__':
     print(years(2006, 2015))
