c48d10a917f8ca68e488e333c24960a3c3b7267b
RITTER <br440r@att.com>
2017-05-10 13:05:57 -0500

    Add a shell script with some key fixes for VS Code for Linux

---
+++ vscode-setup.sh
@@ -0,0 +1,7 @@
+# Things you might want to do to set up VS Code for Linux
+
+# You might need this to install python extensions for vs code
+code --install-extension donjayamanne.python
+
+# Enable alt-click to add multiple cursors
+gsettings set org.gnome.desktop.wm.preferences mouse-button-modifier "<Super>"
