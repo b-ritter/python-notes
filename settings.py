3e09a1e455b6ce889334853f679384a862b1a690
RITTER <br440r@att.com>
2017-05-11 10:03:07 -0500

    Add settings file to set the project root. Create test to check that 'opener' is given a valid file path to open

---
+++ settings.py
@@ -0,0 +1,4 @@
+""" Sets the root path of the project """
+import os
+
+ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
--- systemops/test_opener.py
+++ systemops/test_opener.py
@@ -1,17 +1,23 @@
 import unittest
 import json
+import os
 from unittest.mock import patch, MagicMock, mock_open
 
 from systemops.opener import get_stooges
+import settings
 
 class TestOpener(unittest.TestCase):
     """ Tests opening and retrieving data from json """
 
     @patch('systemops.opener.open', mock_open(read_data='{"beer":"duff"}'))
     def test_get_stooges(self):
+        """ Checks whether the json getter returns the expected value """
         result = get_stooges('homer.json')
         expected_result = json.loads('{"beer":"duff"}')
         self.assertEqual(expected_result, result)
+
+    def test_data_exists(self):
+        self.assertTrue(get_stooges(os.path.join(settings.ROOT_DIR, 'systemops/data/stooges.json')))
 
 if __name__ == "__main__":
     unittest.main()
