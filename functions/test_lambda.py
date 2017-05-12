d32e1d8349c115027e3095d61f8fa882fca1ab52
RITTER <br440r@att.com>
2017-05-12 14:36:22 -0500

    Add a test to generate a range of functions with closures. Check if it returns the expected value

--- functions/test_lambda.py
+++ functions/test_lambda.py
@@ -5,15 +5,28 @@ import random
 
 class TestGetWebsites(unittest.TestCase):
 
-    def test_closure(self, m):
+    def test_closure(self):
         """ See that python supports closures similar to JavaScript """
         def gibberish():
             """ Some random string """
             return ''.join([random.choice(string.ascii_letters) for i in range(20)])
         value1 = gibberish()
-        value2 = gibberish()
-        result = (lambda x: lambda y: x)(value1)(value2)
+        result = (lambda x: lambda: x)(value1)()
         self.assertEqual(result, value1)
+
+    def test_closure_ids(self):
+        """ Show how a variable passed to a function remains in scope
+        even after it returns """
+        def make_lambdas(num):
+            """ Build a lambda generator """
+            for i in range(num):
+                func = lambda x: lambda: x
+                yield func(i)
+        functions = list(make_lambdas(random.randint(1, 10)))
+        random_index = random.randint(0, len(functions)-1)
+        random_function = functions[random_index]()
+        print("{0} equals {1}".format(random_index, random_function))
+        self.assertEqual(random_function, random_index)
 
 if __name__ == "__main__":
     unittest.main()
