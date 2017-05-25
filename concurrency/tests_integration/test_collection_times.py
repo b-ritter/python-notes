87235b353d732c2415843a49623f036817d2fd19
RITTER <br440r@att.com>
2017-05-25 18:01:07 -0500

    Add reporting of serial and concurrent data collection

--- concurrency/tests_integration/test_collection_times.py
+++ concurrency/tests_integration/test_collection_times.py
@@ -9,24 +9,31 @@ import time
 import unittest
 from unittest.mock import patch, MagicMock
 from bs4 import BeautifulSoup
-from concurrency.get_websites import get_number_of_links
+from concurrency.get_websites import get_number_of_links, get_number_of_links_serial
 
 class TestConcurrency(unittest.TestCase):
 
     def setUp(self):
         self.loadtime = 1
+        self.fake_urls = ['url1','url2', 'url3']
 
     @patch('concurrency.get_websites.BeautifulSoup')
     @patch('concurrency.get_websites.load_url')
     def test_concurrent_slower_than_serial(self, mock_load_url, bs_mock):
         """ Time the collection of data from websites """
-        bs_data = MagicMock(return_value=['link1', 'link2'])
+        bs_data = MagicMock(return_value=["<a href='foo'>Baz</a>"])
         bs_mock.return_value = bs_data
         mock_load_url.side_effect = lambda foo: time.sleep(self.loadtime)
         concurrent_start = time.time()
-        list(get_number_of_links(['url1','url2']))
+        list(get_number_of_links(self.fake_urls))
         concurrent_total = time.time() - concurrent_start
-        print(concurrent_total)
+
+        serial_start = time.time()
+        get_number_of_links_serial(self.fake_urls)
+        serial_total = time.time() - serial_start
+        print("Concurrent collection: {}".format(concurrent_total))
+        print("Serial collection: {}".format(serial_total))
+        self.assertLess(concurrent_total, serial_total)
 
 if __name__ == "__main__":
     unittest.main()
