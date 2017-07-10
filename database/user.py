9442b6f71eca26aac1002ae8e40063073d2122e2
RITTER <br440r@att.com>
2017-07-10 11:58:26 -0500

    Refactor User class out of initialize

---
+++ database/user.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String
+from base import Base
+
+class User(Base):
+    __tablename__ = 'users'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    fullname = Column(String)
+    password = Column(String)
+
+    def __repr__(self):
+        return "<User(name='%s', fullname='%s', password='%s')>" % (
+            self.name, self.fullname, self.password
+        )
